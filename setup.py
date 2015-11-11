5213e9bad3fabbd60a17aa97d20408203f7a5f28
tatsuro-yasukawa <tatsuro-yasukawa@cookpad.com>
2015-11-11 16:32:00 +0900

    Add initial files

---
+++ setup.py
@@ -0,0 +1,25 @@
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+
+setup(
+    name = "pandas-rs",
+    packages = ["pandas-rs"],
+    version = "0.1.0",
+    description = "pandas extension for AWS RedShift (Not Officail library)",
+    author = "Tatsuro Yasukawa",
+    author_email = "mark@diveintomark.org",
+    url = "https://github.com/SamuraiT/pandas-rs",
+    download_url = "",
+    keywords = ["pandas", "RedShift", "sql"],
+    classifiers = [
+        "Programming Language :: Python :: 3",
+        "Development Status :: 4 - Beta",
+        "Environment :: Other Environment",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: GNU Library or Lesser General Public License (LGPL)",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        ]
+    )
